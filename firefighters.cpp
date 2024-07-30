f93f0d1f09016507992327034a1e27f661ee108b
Patrick Cummings <patrick@loupe.team>
2024-07-30 14:23:09 -0600

    Make the first working version of the find algo

--- firefighters.cpp
+++ firefighters.cpp
@@ -4,7 +4,7 @@
 #include <thread>
 #include <chrono>
 
-
+const std::string background = "\u{1F3E0}";
 const std::string fire = "\u{1F525}";
 const std::string firefighter = "\u{1F9D1}";
 
@@ -17,7 +17,7 @@ struct Position {
 class Map {
 public:
     // Constructor to initialize the 2D vector with a given size and initial value
-    Map(int size, const std::string& initialValue) : mapSize(size), mapVector(size, std::vector<std::string>(size, initialValue)) {}
+    Map(int size) : mapSize(size), mapVector(size, std::vector<std::string>(size, background)) {}
 
     // Method to print the 2D vector
     void print() const {
@@ -29,18 +29,33 @@ public:
             std::cout << std::endl;
         }
         std::cout << std::endl; // Add an extra newline for better readability between prints
-        //std::cout.flush(); // Ensure the output is immediately displayed JARRED, why should I add this line?
+        std::cout.flush(); // JARRED: why should I have this line?
     }
 
     // Method to set a specific cell in the 2D vector
-    void findFire(int row, int col, const std::string& value) {
+    void findFire() {
 
-        // Move Firefighter one position closer
+        // TODO: feels like I need error checking here
+        while(firefighterPos.x != firePos.x || firefighterPos.y != firePos.y){
+            Position oldPos = firefighterPos;
+            // Move Firefighter one position closer, always move X first
+            if(firefighterPos.x != firePos.x){
+                firefighterPos.x = firefighterPos.x > firePos.x ? firefighterPos.x-1 : firefighterPos.x+1;
+            }
+            else{
+                firefighterPos.y = firefighterPos.y > firePos.y ? firefighterPos.y-1 : firefighterPos.y+1;
+            }
 
-        // update the game screen
+            // update the game screen
+            clear(oldPos.y, oldPos.x);
+            updateFirefighter();
+            print();
 
-        // wait for a set amount of time
+            // wait for a set amount of time at 30FPS-ish
+            std::this_thread::sleep_for(std::chrono::milliseconds(30));
 
+        }
+        std::cout << std::endl;
     }
 
     // Method to start a fire on the map
@@ -51,10 +66,14 @@ public:
         set(firePos.y, firePos.x, fire);
     }
 
-    // Method to set a specific cell in the 2D vector
+    // Method to add a firefighter!
     void addFirefighter(int row, int col, const std::string& value) {
         firefighterPos.x = col;
         firefighterPos.y = row;
+        set(firefighterPos.y, firefighterPos.x, firefighter);
+    }
+
+    void updateFirefighter() {
         set(firefighterPos.y, firefighterPos.x, firefighter);
     }
 
@@ -62,6 +81,15 @@ public:
     void set(int row, int col, const std::string& value) {
         if (row >= 0 && row < mapSize && col >= 0 && col < mapSize) {
             mapVector[row][col] = value;
+        } else {
+            std::cerr << "Index out of bounds" << std::endl;
+        }
+    }
+
+    // Method to clear a specific cell in the 2D vector
+    void clear(int row, int col) {
+        if (row >= 0 && row < mapSize && col >= 0 && col < mapSize) {
+            mapVector[row][col] = background;
         } else {
             std::cerr << "Index out of bounds" << std::endl;
         }
@@ -90,12 +118,12 @@ int main() {
     // Get map size
     int mapSize;
     std::string userIn = "";
-    std::cout << "Welcome to FireFighters! Choose your map size (Min 2, Max 50):";
+    std::cout << "Welcome to FireFighters! Choose your map size (Min 2, Max 30):";
     std::cin >> mapSize;
     mapSize = mapSize < 2 ? 2 : mapSize;
-    mapSize = mapSize > 50 ? 50 : mapSize;
+    mapSize = mapSize > 30 ? 30 : mapSize;
 
-    Map map(mapSize, "-");
+    Map map(mapSize);
 
     map.print();
 
@@ -111,11 +139,8 @@ int main() {
         map.addFirefighter(0, 0, firefighter);
 
         // have the firefighter go to the fire
-        map.print();
+        map.findFire();
 
-
-        // Move at 30FPS-ish
-        std::this_thread::sleep_for(std::chrono::milliseconds(30));
     }
 
 
